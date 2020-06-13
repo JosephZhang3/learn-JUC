8aea00ffa7dc447efe0f386da3070329f942b5fe
zhangjianghao <jianghaozhang@qq.com>
2020-06-13 21:19:16 +0800

    understand thread

--- src/main/java/chap2/CustomTimer.java
+++ src/main/java/chap2/CustomTimer.java
@@ -12,15 +12,18 @@ public class CustomTimer {
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             public void run() {
+                System.out.println("定时任务线程 id: " + Thread.currentThread().getId() + "\tname: " + Thread.currentThread().getName());
                 schedule();
             }
         };
         System.out.println("run到了这里");
+        System.out.println("主线程 id: " + Thread.currentThread().getId() + "\tname: " + Thread.currentThread().getName());
         timer.schedule(task, 5000);
     }
 
     private static void schedule() {
         System.out.println("定时器执行了一次");
+        System.exit(0);
     }
 
 }
