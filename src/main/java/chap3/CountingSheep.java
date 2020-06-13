cc0d1dfac940ba3d8db625c468a34b752ebe0d40
zhangjianghao <jianghaozhang@qq.com>
2020-06-13 20:30:21 +0800

    counting sheep

--- src/main/java/chap3/CountingSheep.java
+++ src/main/java/chap3/CountingSheep.java
@@ -1,5 +1,16 @@
 package chap3;
 
-public class CountingSheep {
+public class CountingSheep extends Thread {
+    volatile boolean asleep;
 
+    @Override
+    public void run() {
+        while (!asleep) {
+            countSomeSheep();
+        }
+    }
+
+    void countSomeSheep() {
+        System.out.println("I see one sheep");
+    }
 }
--- src/test/java/chap3/CountingSheepTest.java
+++ src/test/java/chap3/CountingSheepTest.java
@@ -2,6 +2,9 @@ package chap3;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class CountingSheepTest {
 
     @Test
