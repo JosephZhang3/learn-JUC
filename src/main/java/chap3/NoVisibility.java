a14ecbe7e586bee36cf882e0d815ebfd4c064f05
zhangjianghao <jianghaozhang@qq.com>
2020-06-13 20:07:44 +0800

    refactor

---
+++ src/main/java/chap3/NoVisibility.java
@@ -0,0 +1,24 @@
+package chap3;
+
+public class NoVisibility {
+
+    private static boolean ready;
+    private static int number;
+
+    private static class ReaderThread extends Thread {
+        @Override
+        public void run() {
+            while (!ready) {
+                Thread.yield();//放弃
+            }
+            System.out.println("current number is " + number);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReaderThread().start();
+        number = 42;
+        ready = true;
+        // 一般的情况下，输出42，并不会看出来书里面说的"重排序"现象
+    }
+}
---
+++ src/test/java/chap3/CountingSheepTest.java
@@ -0,0 +1,11 @@
+package chap3;
+
+import org.junit.jupiter.api.Test;
+
+public class CountingSheepTest {
+
+    @Test
+    public void testCountingSheep() {
+
+    }
+}
