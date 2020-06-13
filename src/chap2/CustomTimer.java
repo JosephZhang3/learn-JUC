166cb5084c5df325a2531124465533db8570f62d
zhangjianghao <jianghaozhang@qq.com>
2020-06-13 17:50:00 +0800

    内置锁

--- src/chap2/CustomTimer.java
+++ src/chap2/CustomTimer.java
@@ -6,20 +6,20 @@ import java.util.TimerTask;
 /**
  * 自定义定时器
  */
-public class customTimer {
+public class CustomTimer {
 
     public static void main(String[] args) {
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             public void run() {
-                schedual();
+                schedule();
             }
         };
         System.out.println("run到了这里");
         timer.schedule(task, 5000);
     }
 
-    private static void schedual() {
+    private static void schedule() {
         System.out.println("定时器执行了一次");
     }
 
---
+++ src/chap2/section2dot3/SynchronizedFactorize.java
@@ -0,0 +1,64 @@
+package chap2.section2dot3;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.math.BigInteger;
+
+/**
+ * 虽然是完全线程安全的，但是会有线程活跃性问题
+ *
+ * @author jianghao.zhang
+ */
+@ThreadSafe
+public class SynchronizedFactorize implements Servlet {
+    @GuardedBy("this")
+    private BigInteger lastNumber;
+    @GuardedBy("this")
+    private BigInteger[] lastFactors;
+
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    /**
+     * 注意：使用了同步关键字 synchronized
+     */
+    public synchronized void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        BigInteger i = extractFromRequest(servletRequest);
+        if (lastNumber.equals(i)) {
+            encodeIntoResponse(servletResponse, lastFactors);
+        }
+        BigInteger[] factors = factor(i);
+        // 重置缓存
+        lastNumber = i;
+        lastFactors = factors;
+        encodeIntoResponse(servletResponse, factors);
+    }
+
+    public String getServletInfo() {
+        return null;
+    }
+
+    public void destroy() {
+
+    }
+
+    BigInteger extractFromRequest(ServletRequest req) {
+        return new BigInteger("7");
+    }
+
+    BigInteger[] factor(BigInteger i) {
+        // Doesn't really factor
+        return new BigInteger[]{i};
+    }
+
+    void encodeIntoResponse(ServletResponse res, BigInteger[] factors) {
+    }
+}
