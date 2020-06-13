cd4b2b0bfefa2135bd0b24835588d83a845bae26
zhangjianghao <jianghaozhang@qq.com>
2020-06-13 21:48:32 +0800

    hashCode

--- src/test/java/chap3/CountingSheepTest.java
+++ src/test/java/chap3/CountingSheepTest.java
@@ -9,6 +9,15 @@ public class CountingSheepTest {
 
     @Test
     public void testCountingSheep() {
+        // junit 测试方法里的所有对象都会被它代理，因此这里调用hashCode方法传递给String类的参数
+        // 并不是fdsf..字符串，而是junit执行器的类名
+        int hashCode = "fdsfdsf我是谁dsfsd".hashCode();
+        System.out.println(hashCode);
+    }
 
+    public static void main(String[] args) {
+        // 思考：hashCode 算法的乘法基数为什是 31 ，即 2^31 -1
+        int hashCode = "fdsfdsf我是谁dsfsd".hashCode();
+        System.out.println(hashCode);
     }
 }
