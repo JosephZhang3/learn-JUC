26cf6a6dc48ec681f92d313b5e141642cfa506b4
jianghao.zhang <jianghaozhang@qq.com>
2020-07-15 11:43:47 +0800

    BetterVector 扩展类来增加线程安全的方法 ListHelper 通过客户端加锁机制来增加线程安全的方法

---
+++ src/main/java/chap4/BetterVector.java
@@ -0,0 +1,25 @@
+package chap4;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.Vector;
+
+@ThreadSafe
+public class BetterVector<E> extends Vector<E> {
+
+    /**
+     * 向量列表中不存在元素时，添加
+     * 这个方法签名使用的修饰符 synchronized
+     *
+     * @param x
+     * @return
+     */
+    public synchronized boolean putIfAbsent(E x) {
+        boolean absent = !super.contains(x);
+        if (absent) {
+            super.add(x);
+        }
+        return absent;
+    }
+
+}
