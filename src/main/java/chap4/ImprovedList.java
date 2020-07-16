937c354ad92e753d9d345e372a687affdb01a929
jianghao.zhang <jianghaozhang@qq.com>
2020-07-16 09:47:41 +0800

    通过 composition 添加原子操作

---
+++ src/main/java/chap4/ImprovedList.java
@@ -0,0 +1,190 @@
+package chap4;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+@ThreadSafe
+public class ImprovedList<T> implements List<T> {
+    private final List<T> list;
+
+    public ImprovedList(List<T> list) {
+        this.list = list;
+    }
+
+    /**
+     * 原子操作方法，非 override
+     *
+     * @param t
+     * @return
+     */
+    public synchronized boolean putIfAbsent(T t) {
+        boolean absent = !list.contains(t);
+        if (absent) {
+            list.add(t);
+        }
+        return absent;
+    }
+
+    @Override
+    public synchronized void clear() {
+        list.clear();
+    }
+
+    @Override
+    public synchronized boolean add(T t) {
+        return list.add(t);
+    }
+
+    /**
+     * 这个方法JDK 8 才有？？？
+     *
+     * @param index
+     * @param element
+     */
+    @Override
+    public synchronized void add(int index, T element) {
+
+    }
+
+    @Override
+    public synchronized boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    //----------------- 同步方法结束 -----------------
+
+    @Override
+    public synchronized T get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        list.forEach(action);
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return list.spliterator();
+    }
+
+    @Override
+    public Stream<T> stream() {
+        return list.stream();
+    }
+
+    @Override
+    public Stream<T> parallelStream() {
+        return list.parallelStream();
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super T> filter) {
+        return list.removeIf(filter);
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<T> operator) {
+        list.replaceAll(operator);
+    }
+
+    @Override
+    public void sort(Comparator<? super T> c) {
+        list.sort(c);
+    }
+}
