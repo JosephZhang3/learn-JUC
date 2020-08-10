2b94c908be2b4b4a5fcd0d499588438dbd3ea44c
jianghao.zhang <jianghaozhang@qq.com>
2020-08-10 17:22:40 +0800

    rename

--- src/main/java/chap5/ModifyExceptionTest.java
+++ src/main/java/chap5/ModifyExceptionTest.java
@@ -3,7 +3,7 @@ package chap5;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ModifyExceptio {
+public class ModifyExceptionTest {
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
